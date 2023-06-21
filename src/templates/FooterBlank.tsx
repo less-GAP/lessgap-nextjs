5cc7b6bc2f55d3cbc8ce3e4f65933b221c8a151c
MADELab Dev <baonguyenyam@gmail.com>
2023-06-20 19:36:59 -0500

    s

--- src/templates/FooterBlank.tsx
+++ src/templates/FooterBlank.tsx
@@ -5,10 +5,7 @@ import MadeLabFooter from "@/components/MadeLabFooter";
 const MadeFooterBlank = () => (
 	<MadeLabFooter>
 		<Script id="script" type="text/javascript">
-			{`
-        var $zoho = $zoho || {}; 
-        $zoho.salesiq = $zoho.salesiq || { widgetcode: "e5fd57a9e647943ff36870624f02c640ff0ab33bfe67881491eea981ea610171", values: {}, ready: function () { $zoho.salesiq.floatbutton.visible('hide'); } }; var d = document; s = d.createElement("script"); s.type = "text/javascript"; s.id = "zsiqscript"; s.defer = true; s.src = "https://salesiq.zoho.com/widget"; t = d.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t);
-      `}
+			{``}
 		</Script>
 	</MadeLabFooter>
 );
